80854c186f8c85af0f9c05d2d0a351f0660c1dc0
dawwik <dawid.wiktorowski22@gmail.com>
2020-03-29 17:39:06 +0200

    Dodanie tła i loga oraz muzyki rozgrywki

--- src/StartWidok.java
+++ src/StartWidok.java
@@ -13,11 +13,11 @@ public class StartWidok extends JFrame {
     private JButton buttonRanking;
     private Clip clip;
 
-    public StartWidok(){
+    public StartWidok() {
         setLayout(null);
         setVisible(true);
-        setLocation(500,250);
-        setSize(800,600);
+        setLocation(500, 250);
+        setSize(800, 600);
         setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         setIconImage(new ImageIcon("image\\chicken.png").getImage());
 
@@ -31,13 +31,13 @@ public class StartWidok extends JFrame {
         background.setBounds(0, -10, 800, 600);
         add(background);
 
-        buttonStart.setBounds(200,300,200,200);
+        buttonStart.setBounds(200, 300, 200, 200);
         buttonStart.setIcon(new ImageIcon("image\\play.png"));
         buttonStart.setOpaque(false);
         buttonStart.setContentAreaFilled(false);
         buttonStart.setBorderPainted(false);
 
-        buttonRanking.setBounds(400, 300,200,200);
+        buttonRanking.setBounds(400, 300, 200, 200);
         buttonRanking.setIcon(new ImageIcon("image\\rank.png"));
         buttonRanking.setOpaque(false);
         buttonRanking.setContentAreaFilled(false);
@@ -48,6 +48,7 @@ public class StartWidok extends JFrame {
             rankingMap.put(nickname, 0);
             clip.stop();
             musicAttack();
+            melodyOfTheGame();
             dispose();
             new GameWidok().setVisible(true);
         });
@@ -55,17 +56,17 @@ public class StartWidok extends JFrame {
         buttonRanking.addActionListener(e -> {
             int row = 0;
             JDialog jDialog = new JDialog(this, "Ranking");
-            Map<String, Integer> sort = rankingMap.entrySet().stream().sorted(((o1, o2) -> o2.getValue().compareTo(o1.getValue()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o1,o2) -> o1, LinkedHashMap::new));
-            JTable jTable = new JTable(sort.size(),2);
-            for (String key : sort.keySet()){
+            Map<String, Integer> sort = rankingMap.entrySet().stream().sorted(((o1, o2) -> o2.getValue().compareTo(o1.getValue()))).collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue, (o1, o2) -> o1, LinkedHashMap::new));
+            JTable jTable = new JTable(sort.size(), 2);
+            for (String key : sort.keySet()) {
                 jTable.setValueAt(key, row, 0);
                 jTable.setValueAt(sort.get(key), row, 1);
                 row++;
             }
             jDialog.setIconImage(new ImageIcon("image\\rank1.png").getImage());
             jDialog.add(jTable);
-            jDialog.setSize(300,500);
-            jDialog.setLocation(1000,250);
+            jDialog.setSize(300, 500);
+            jDialog.setLocation(1000, 250);
             jDialog.setVisible(true);
         });
 
@@ -75,7 +76,7 @@ public class StartWidok extends JFrame {
         add(background);
     }
 
-    private void music(){
+    private void music() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("music\\imperial_march.wav").getAbsoluteFile());
             clip = AudioSystem.getClip();
@@ -86,7 +87,7 @@ public class StartWidok extends JFrame {
         }
     }
 
-    private void musicAttack(){
+    private void musicAttack() {
         try {
             AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("music\\chicken2.wav").getAbsoluteFile());
             clip = AudioSystem.getClip();
@@ -94,6 +95,17 @@ public class StartWidok extends JFrame {
             clip.start();
         } catch (IOException | UnsupportedAudioFileException | LineUnavailableException s) {
             s.printStackTrace();
+        }
+    }
+
+    private void melodyOfTheGame() {
+        try {
+            AudioInputStream audioInputStream = AudioSystem.getAudioInputStream(new File("C:\\Users\\dawid\\OneDrive\\Pulpit\\Projekt\\Chicken-Invaders\\music\\muzyka rozpoczynająca rozgrywkę.wav").getAbsoluteFile());
+            clip = AudioSystem.getClip();
+            clip.open(audioInputStream);
+            clip.start();
+        } catch (IOException | UnsupportedAudioFileException | LineUnavailableException d) {
+            d.printStackTrace();
         }
     }
 }
